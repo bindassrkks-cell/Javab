4497e7aadbe45d13e5092dc27ac038d6238cb6c5
bindassrkks-cell <bindassrkks@gmail.com>
2026-06-05 10:50:31 +0530

    Update AdminReceiver.java

--- app/src/main/java/com/zero/xploid/AdminReceiver.java
+++ app/src/main/java/com/zero/xploid/AdminReceiver.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class AdminReceiver extends DeviceAdminReceiver {
+    
     @Override
     public void onEnabled(Context context, Intent intent) {
-        Toast.makeText(context, "Device Admin: enabled", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Device Admin Enabled", Toast.LENGTH_LONG).show();
     }
-
+    
     @Override
     public void onDisabled(Context context, Intent intent) {
-        Toast.makeText(context, "Device Admin: disabled", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Device Admin Disabled", Toast.LENGTH_SHORT).show();
     }
 }
