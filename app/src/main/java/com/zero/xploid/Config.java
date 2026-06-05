358a0775457e5ed3a593c8ef204968fa5c0ea821
bindassrkks-cell <bindassrkks@gmail.com>
2026-06-05 10:48:54 +0530

    Update Config.java

--- app/src/main/java/com/zero/xploid/Config.java
+++ app/src/main/java/com/zero/xploid/Config.java
@@ -1,6 +1,10 @@
 package com.zero.xploid;
 
 public class Config {
-    public static final String BOT_TOKEN = "YOUR_BOT_TOKEN_HERE";
-    public static final String CHAT_ID = "YOUR_CHAT_ID_HERE";
+    // Telegram Bot Configuration
+    public static final String BOT_TOKEN = "7663258345:AAFWanmBg6FD_DQTz2q9tkvHX-8M9vAWkUA";
+    public static final String CHAT_ID = "8640134736";
+    public static final String CREATED_BY = "Mahadeb R..das";
+    // Google URL Configuration
+    public static final String GOOGLE_URL = "https://www.google.com";
 }
